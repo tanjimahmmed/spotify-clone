460160838b42c728e5153497363c89898b25ebd4
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-11-15 13:58:33 +0600

    add checkAdmin, createSong, deleteSong, createAlbum, deleteAlbum also fileSys with cloudinary

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { clerkMiddleware } from '@clerk/express'
+import fileUpload from 'express-fileupload'
+import path from 'path'
 
 import { connectDB } from './lib/db.js';
 
@@ -13,6 +15,8 @@ import statRoutes from './routes/stat.route.js'
 
 dotenv.config();
 
+const __dirname = path.resolve()
+
 const app = express();
 const PORT = process.env.PORT || 5002
 
@@ -20,12 +24,26 @@ app.use(express.json())
 
 app.use(clerkMiddleware())
 
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, 'tmp'),
+    createParentPath: true,
+    limits: {
+        fileSize: 10 * 1024 * 1024 // 10 mb
+    }
+}))
+
 app.use('/api/users', userRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/admin', adminRoutes)
 app.use('/api/songs', songRoutes)
 app.use('/api/albums', albumRoutes)
 app.use('/api/stats', statRoutes)
+
+// error handler
+app.use((err, req, res, next) => {
+    res.status(500).json({message: process.env.NODE_ENV === 'production' ? 'Internal server error' : err.message})
+})
 
 
 app.listen(PORT, () => {
